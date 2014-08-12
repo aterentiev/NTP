6af94e481ce277db00d6f24d772ad696efe7ef64
Alexandre Terentiev <aterentiev@users.noreply.github.com>
2014-08-12 09:15:18 +0200

    Update NTP.cpp

--- NTP.cpp
+++ NTP.cpp
@@ -2,9 +2,8 @@
 
 NTP::NTP()
 {
-    _UDP = new EthernetUDP;
-    _DNS = new DNSClient; 
     _callback = 0;
+    _resolved = false;
 }
 
 NTP::~NTP()
@@ -15,8 +14,6 @@ void NTP::Initialize(const char *server, unsigned long period)
 {
     _server = server;
     _period = period;
-    _UDP->begin(NTP_CLIENT_PORT);
-    _DNS->begin(Ethernet.dnsServerIP());
 }
 
 void NTP::forceOnce()
@@ -24,7 +21,7 @@ void NTP::forceOnce()
     _last_call = millis() - _period - 1; // Guaranteed call now
 }
 
-void NTP::attachInterrupt(NTPServerCallbackType callback)
+void NTP::attachInterrupt(NTPServerResponseCallbackType callback)
 {
     _callback = callback;
 }
@@ -38,9 +35,13 @@ void NTP::Do()
 {
     // Simple "call-answer after timeout" state machine
     if (abs(millis() - _last_call) > _period) {
-        _Request();
+        if (_Resolve()) {
+            _UDP = new EthernetUDP;
+            _UDP->begin(NTP_CLIENT_PORT);
+            _Request();
+            _requested = true;
+        }
         _last_call = millis();
-        _requested = true;
     }
     if ((_requested) && (abs(millis() - _last_call) > NTP_RESPONSE_TIMEOUT)) {
         if (_UDP->parsePacket()) {
@@ -51,35 +52,73 @@ void NTP::Do()
             unsigned long lowWord = word(_PacketBuffer[42], _PacketBuffer[43]);  
             unsigned long secsSince1900 = highWord << 16 | lowWord;	 		
             const unsigned long seventyYears = 2208988800UL;	 
-            uint32_t epoch = secsSince1900 - seventyYears;
+            unsigned long epoch = secsSince1900 - seventyYears;
             if (_callback) _callback(epoch);
         } else {
             // No answer within timeout, maybe server unavailable? Return 0
             if (_callback) _callback(0);
         }
         _requested = false;
+        _UDP->stop();
+        delete _UDP;
     }
+}
+
+bool NTP::_Resolve()
+{
+    bool _successful = true;
+    IPAddress _DNSIP;
+    _DNS = new DNSClient;
+    _DNS->begin(Ethernet.dnsServerIP());
+    int _DNSResult = _DNS->getHostByName(_server, _DNSIP);
+    if (_DNSResult == DNS_SUCCESS) {
+        Serial.print(F("NTPClient: DNS resolving successful: ")); Serial.println(_DNSIP);
+        _IP = _DNSIP;
+        _resolved = true;
+    } else {
+        Serial.print(F("NTPClient: DNS resolving not successful, error code: "));
+        switch (_DNSResult) {
+            case DNS_TIMED_OUT:
+                Serial.print(F("TIMED_OUT, ")); break;
+            case DNS_INVALID_SERVER:
+                Serial.print(F("INVALID_SERVER, ")); break;
+            case DNS_TRUNCATED:
+                Serial.print(F("TRUNCATED, ")); break;
+            case DNS_INVALID_RESPONSE:
+                Serial.print(F("INVALID_RESPONSE, ")); break;
+            default:
+                Serial.print(F("UNKNOWN, "));
+        }
+        if (_resolved) {
+            Serial.print(F("using last known IP address: ")); Serial.println(_IP);
+        } else {
+            // Can happen only directly after restart if no resolving was made earlier
+            Serial.println("waiting for the next time, no NTP requset will be sent");
+            _successful = false;
+        }
+    }
+    delete _DNS;
+    _DNS = 0;
+    return _successful;
 }
 
 void NTP::_Request()
 {
-    if (_DNS->getHostByName(_server, _IP) == 1) {
-        // clear all bytes in the buffer
-        memset(_PacketBuffer, 0, NTP_PACKET_SIZE); 
+    // clear all bytes in the buffer
+    memset(_PacketBuffer, 0, NTP_PACKET_SIZE); 
 
-        // Initialize values needed to form NTP request
-        _PacketBuffer[0] = 0b11100011;  // LI, Version, Mode
-        _PacketBuffer[1] = 0; // Stratum, or type of clock
-        _PacketBuffer[2] = 6; // Polling Interval
-        _PacketBuffer[3] = 0xEC; // Peer Clock Precision
-        // 8 bytes of zero for Root Delay & Root Dispersion
-        _PacketBuffer[12] = 49; 
-        _PacketBuffer[13] = 0x4E;
-        _PacketBuffer[14] = 49;
-        _PacketBuffer[15] = 52;
+    // Initialize values needed to form NTP request
+    _PacketBuffer[0] = 0b11100011;  // LI, Version, Mode
+    _PacketBuffer[1] = 0; // Stratum, or type of clock
+    _PacketBuffer[2] = 6; // Polling Interval
+    _PacketBuffer[3] = 0xEC; // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    _PacketBuffer[12] = 49; 
+    _PacketBuffer[13] = 0x4E;
+    _PacketBuffer[14] = 49;
+    _PacketBuffer[15] = 52;
 	
-        _UDP->beginPacket(_IP, 123); 
-        _UDP->write(_PacketBuffer, NTP_PACKET_SIZE);
-        _UDP->endPacket(); 
-    }
+    _UDP->beginPacket(_IP, 123); 
+    _UDP->write(_PacketBuffer, NTP_PACKET_SIZE);
+    _UDP->endPacket(); 
 }
