d54eb9be949cfc7bb2711aa392eed254707821fd
Alexandre Terentiev <aterentiev@users.noreply.github.com>
2014-10-15 11:34:46 +0200

    Update NTP.h

--- NTP.h
+++ NTP.h
@@ -6,6 +6,9 @@
 #include <EthernetUdp.h>
 #include "Dns.h"
 
+// Comment this to suppress serial debug output (saves appx. 500 bytes flash)
+#define NTP_DEBUG 1
+
 #define NTP_CLIENT_PORT 8888
 #define NTP_PACKET_SIZE 48
 #define NTP_RESPONSE_TIMEOUT 1000
@@ -23,8 +26,9 @@ class NTP
         NTP();
         ~NTP();
 
-        void Initialize(const char *, unsigned long);
+        void Initialize(const char *, long);
         void forceOnce();
+        void setPeriod(long);
 
         typedef void (*NTPServerResponseCallbackType)(unsigned long);
 
@@ -38,8 +42,9 @@ class NTP
 
         byte _PacketBuffer[NTP_PACKET_SIZE];
 
+        bool _forced;
         unsigned long _last_call;
-        unsigned long _period;
+        long _period;
         const char *_server;
 
         // Callback placeholder
